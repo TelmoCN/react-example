8db8d579f9489ea7e60f7591786ddf21a5b2d1df
Telmo Sampaio <telmo.sampaio@wearecodenation.com>
2019-11-21 10:30:29 +0000

    Updates

---
+++ precache-manifest.0f60c095e4dddc72b2846df8d84bafcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e977210e9a7f776d233f7a4a7ef25f6e",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "eef254ebd162529eef81",
+    "url": "/react-example/static/css/main.b4d6b7bd.chunk.css"
+  },
+  {
+    "revision": "90f739edab528aa627f6",
+    "url": "/react-example/static/js/2.5901f9c8.chunk.js"
+  },
+  {
+    "revision": "eef254ebd162529eef81",
+    "url": "/react-example/static/js/main.d3f7519a.chunk.js"
+  },
+  {
+    "revision": "8a6580b10e43d7326a5e",
+    "url": "/react-example/static/js/runtime-main.94e0a0b6.js"
+  }
+]);
